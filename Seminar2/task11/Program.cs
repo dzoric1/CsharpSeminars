c114bc8d5faea94faddc55ac9de74d8bef0e86f9
dzoric1 <Gerasimov2099@mail.ru>
2022-09-06 16:12:10 +0800

    realized task11

---
+++ Seminar2/task11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+System.Console.WriteLine($"Сгенерировалось случайное число {number}");
+int digit2 = number % 10;
+int digit1 = number / 100;
+
+System.Console.WriteLine($"Число без второлй цифры {digit1}{digit2}");
+
+
