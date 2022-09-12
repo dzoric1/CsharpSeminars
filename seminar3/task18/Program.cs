32a07433e9e4dafbd6d2613df8c193bf81c5f671
dzoric1 <Gerasimov2099@mail.ru>
2022-09-12 13:36:23 +0800

    realized task 18

---
+++ seminar3/task18/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+   System.Console.WriteLine("x > 0 и y > 0");
+}
+else if (number == 2)
+{
+   System.Console.WriteLine("x < 0 и y > 0");
+}
+else if (number == 3)
+{
+   System.Console.WriteLine("x < 0 и y < 0");
+}
+else if (number == 4)
+{
+   System.Console.WriteLine("x > 0 и y < 0");
+}
+else
+{
+   System.Console.WriteLine("Номер четверти задан неправильно");
+}
