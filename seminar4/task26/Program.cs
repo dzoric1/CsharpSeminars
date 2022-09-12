d3ab074e1b7e52a42d31ab4a861a3325c6bf87d8
dzoric1 <Gerasimov2099@mail.ru>
2022-09-12 19:12:08 +0800

    realized task26 and task 28

---
+++ seminar4/task26/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+
+while (number > 0)
+{
+   number = number / 10;
+   i++;
+}
+System.Console.WriteLine(i);
---
+++ seminar4/task28/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+for (int i = 1; number >= i; i++)
+{
+   result = result * i;
+}
+
+System.Console.WriteLine(result);
