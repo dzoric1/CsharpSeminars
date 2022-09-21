6418d66c440cf8733e16c54e3289a6bcc3bf2e27
dzoric1 <Gerasimov2099@mail.ru>
2022-09-21 15:23:19 +0800

    realized task 44

---
+++ seminar6/task44/Program.cs
@@ -0,0 +1,26 @@
+﻿int ReadNumber(string message)
+{
+   Console.WriteLine(message);
+   return Convert.ToInt32(Console.ReadLine());
+}
+
+void FibonachiN(int n)
+{
+   int number1 = 0;
+   int number2 = 1;
+   int temp = 0;
+   System.Console.Write(number1 + " ");
+   System.Console.Write(number2 + " ");
+
+   for (int i = 2; i < n; i++)
+   {
+      temp = number1 + number2;
+      System.Console.Write(temp + " ");
+      number1 = number2;
+      number2 = temp;
+
+   }
+}
+
+int N = ReadNumber("Введите число");
+FibonachiN(N);
